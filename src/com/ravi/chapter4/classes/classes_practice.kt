7f4133c0e6b582564949dfb4e92a18f7fd4a64da
Ravi Khakhkhar <rkhakhkhar@equalexperts.com>
2021-01-27 17:12:16 +0530

    classes file

--- src/com/ravi/chapter4/classes/classes_practice.kt
+++ src/com/ravi/chapter4/classes/classes_practice.kt
@@ -4,10 +4,20 @@ class User(_nickname: String) {
     val nickname = _nickname
 }
 
-open class User1(nickname: String, isSubscribed: Boolean = true)
+open class User1(val nickname: String, val isSubscribed: Boolean = true)
 
 class TwitterUser(handle: String, nickname: String) : User1(nickname) {
 
+}
+
+class View {
+    val name: String
+    constructor(name: User) {
+        this.name = name.nickname
+    }
+    constructor(ctx: User1) {
+        this.name = ctx.nickname
+    }
 }
 
 fun main(args: Array<String>) {
@@ -16,5 +26,7 @@ fun main(args: Array<String>) {
 
     val user1 = User1("Alice")
     val user2 = User1("bob", false)
+    val twitterUser = TwitterUser("handle one", "nickname")
+
 
 }
